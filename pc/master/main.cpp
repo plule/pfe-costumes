a5b5b149d553b0ef64f7168e285241826397c96e
Pierre Lulé <pierre@lule.fr>
2013-04-22 10:49:35 +0200

    autodetect translation with resource

--- pc/master/main.cpp
+++ pc/master/main.cpp
@@ -17,18 +17,8 @@ int main(int argc, char *argv[])
     a.setApplicationName("pfe-costumes");
     a.setOrganizationName("INSA-CNCS-MJCSteFoy");
     QTranslator translator;
-    QLocale locale;
-
-    //qDebug() << QDir(a.applicationDirPath()).filePath("master_fr");
-    //translator.load(QDir(a.applicationDirPath()).filePath("master_fr"));
-    translator.load(":/translations/master_fr");
+    translator.load(":/translations/master");
     a.installTranslator(&translator);
-    /*foreach(QString strlocale, locale.uiLanguages()) {
-        if(translator.load(QDir(a.applicationDirPath()).filePath("master_"+strlocale))) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }*/
 
     MainWindow w;
     w.show();
