d68c9c148c3857a0c098b2b59a3cb0a2ef3f08e5
Pierre Lulé <pierre@lule.fr>
2013-05-06 15:12:16 +0200

    arduino save servo state

--- arduino/morpho/src/app.cpp
+++ arduino/morpho/src/app.cpp
@@ -1,8 +1,14 @@
 #include "../../common/communication.h"
 #include <Servo.h>
+#include <avr/eeprom.h>
 
 //Servo servo;
 Servo servos[MOTOR_NUMBER];
+
+unsigned long lastSave;
+unsigned long savePeriod = 2000;
+
+#define EEPROM_SERVO 0
 
 
 #define X(pin, define, string) pin,
@@ -13,6 +19,24 @@ int morpho_pins[] = {
 
 int led=13;
 
+uint16_t *servoAdress(int index)
+{
+    return (uint16_t *)EEPROM_SERVO+sizeof(int)*index;
+}
+
+void saveState()
+{
+    DBG("Saving...");
+    int i;
+    for(i=0; i<MOTOR_NUMBER; i++) {
+        uint16_t value = eeprom_read_word(servoAdress(i));
+        if(value != servos[i].readMicroseconds()) {
+            DBG("Value changed");
+            eeprom_write_word(servoAdress(i), servos[i].readMicroseconds());
+        }
+    }
+}
+
 void setup()
 {
     pinMode(led, OUTPUT);
@@ -22,6 +46,7 @@ void setup()
     int i;
     for(i=0; i<MOTOR_NUMBER; i++) {
         servos[i].attach(morpho_pins[i], MORPHO_MIN, MORPHO_MAX);
+        servos[i].writeMicroseconds(eeprom_read_word(servoAdress(i)));
     }
     //servo.attach(52,800,2200);
 }
@@ -45,4 +70,9 @@ void handleMessage(MSG_TYPE type, int idMsg, int expe, HardwareSerial serial)
 
 void loop()
 {
+    unsigned long time = millis();
+    if(time - lastSave > savePeriod) {
+        saveState();
+        lastSave = time;
+    }
 }
