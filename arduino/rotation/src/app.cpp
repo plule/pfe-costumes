184891ce51c07024d047ce2e7a20d92654a3333c
Pierre Lulé <pierre@lule.fr>
2013-05-07 15:40:31 +0200

    read line of led

--- arduino/rotation/src/app.cpp
+++ arduino/rotation/src/app.cpp
@@ -2,11 +2,15 @@
 //#include "../../../interfaces/interfaces.h"
 //#include "../../common/communication.h"
 
+#define NBLED 8
+#define LIMIT 500
+
 extern HardwareSerial Serial;
 int i=0;
 int ledOn=5;
 
-int ledTest=6;
+int leds[] = {6,7,8,9,10,11,12,13};
+int values[8];
 //int led=13;
 
 void setup()
@@ -23,15 +27,21 @@ void setup()
 void loop()
 {
     unsigned long us;
-    unsigned long delta;
-    pinMode(ledTest, OUTPUT);
-    digitalWrite(ledTest, HIGH);
-    delayMicroseconds(20);
-    us = micros();
-    pinMode(ledTest, INPUT);
-    while(digitalRead(ledTest) == HIGH){};
-    delta = micros() - us;
-    Serial.println(delta);
+    int i;
+    for(i=0; i < NBLED; i++) {
+        int led = leds[i];
+        pinMode(led, OUTPUT);
+        digitalWrite(led, HIGH);
+        delayMicroseconds(20);
+        us = micros();
+        pinMode(led, INPUT);
+        while(digitalRead(led) == HIGH){};
+        values[i] = ((micros()-us) > LIMIT);
+        Serial.print(values[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+    delay(10);
 }
 /*
 void handleMessage(MSG_TYPE type, int idMsg, int expe, HardwareSerial serial)
