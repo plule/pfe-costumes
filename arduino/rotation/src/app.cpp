68bb945e9140cbe62bb6909a6db48c62378a82a5
Pierre Lulé <pierre@lule.fr>
2013-05-07 15:26:29 +0200

    started reading led

--- arduino/rotation/src/app.cpp
+++ arduino/rotation/src/app.cpp
@@ -1,22 +1,41 @@
 #include <Arduino.h>
-#include <Servo.h>
-#include "../../../interfaces/interfaces.h"
+//#include "../../../interfaces/interfaces.h"
+//#include "../../common/communication.h"
 
 extern HardwareSerial Serial;
 int i=0;
-int led=13;
+int ledOn=5;
+
+int ledTest=6;
+//int led=13;
 
 void setup()
 {
-    pinMode(led, OUTPUT);
-    digitalWrite(led, HIGH);
+    //pinMode(led, OUTPUT);
     Serial.begin(9600);
-    digitalWrite(led, LOW);
+    pinMode(ledOn, OUTPUT);
+    digitalWrite(ledOn, HIGH);
+    //digitalWrite(led, HIGH);
+    //init_ard(ROTATION);
+    //digitalWrite(led, LOW);
 }
 
 void loop()
 {
-    Serial.println(i++);
-    delay(500);
-    digitalWrite(led, HIGH);
+    unsigned long us;
+    unsigned long delta;
+    pinMode(ledTest, OUTPUT);
+    digitalWrite(ledTest, HIGH);
+    delayMicroseconds(20);
+    us = micros();
+    pinMode(ledTest, INPUT);
+    while(digitalRead(ledTest) == HIGH){};
+    delta = micros() - us;
+    Serial.println(delta);
 }
+/*
+void handleMessage(MSG_TYPE type, int idMsg, int expe, HardwareSerial serial)
+{
+    //DBG("fio");
+}
+*/
