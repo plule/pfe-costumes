3d6abfdc51611e6be020554be1f3884d06ec796c
Pierre Lulé <pierre@lule.fr>
2015-10-22 16:29:42 +0200

    Implements a more robust mass capture method (still unstable)

--- pc/core/masscapture.cpp
+++ pc/core/masscapture.cpp
@@ -3,6 +3,7 @@
 MassCapture::MassCapture(QObject *parent) :
     QObject(parent)
 {
+    qRegisterMetaType<MassCapture::Problem>( "MassCapture::Problem" );
 }
 
 MassCapture::~MassCapture()
@@ -23,7 +24,7 @@ void MassCapture::massCapture(QPhoto::QCamera *camera, ArduinoCommunication *mor
     m_delayedDownload = delayedDownload;
 
     qDebug() << "Mass Capture object created";
-    if(m_camera->captureToFile("/tmp/dummy.jpg") >= 0) // ensure camera is initialized
+    if(m_camera->captureToFile("/tmp/dummy.jpg", 3) >= 0) // ensure camera is initialized
         launchMassCapture();
     else {
         qDebug() << "failed to initialize camera";
@@ -43,61 +44,68 @@ void MassCapture::setCamera(QPhoto::QCamera *camera)
 
 void MassCapture::launchMassCapture()
 {
-    qDebug() << "camera synced and capture launched";
-    m_index = 0;
+    qDebug() << "launching mass capture";
+
+    // List of photo index to capture
+    m_captureIndexes.clear();
+    for(int i=0; i<m_target; i++)
+        m_captureIndexes.insert(i);
+
+    // Position of the turntable
+    m_captureIndex = 0;
+    m_currentIndex = 0;
+
+    // Prepare the capture timer
     m_captureTimer->setSingleShot(false);
     m_captureTimer->setInterval((1000 * m_rotationTime) / m_settings.value(S_PHOTONUMBER).toInt()); // delay between each shot in ms
 
+    // Track beginning and end of a capture
+    m_readyForNextCapture = true;
+
+    // Timer called at each interesting capture position
     connect(m_captureTimer, &QTimer::timeout, [=]() {
-        qDebug() << "launching capture";
-        if(m_index >= m_target) {
-            qDebug() << "capture finished without problem";
-            m_captureTimer->stop();
+        // Update turntable position
+        m_currentIndex = (m_currentIndex + 1)%m_target;
 
-            if(m_delayedDownload) {
-                qDebug() << "downloading";
-                for(int i = 0; i < m_cameraPaths.size(); i++) {
-                    QString localPath = m_collection->getFilePath(m_idCostume, "turntable", "jpg", i);
-                    QFile localFile(localPath);
-                    qDebug() << localPath;
-                    qDebug() << m_cameraPaths.value(i);
-                    m_camera->getFile(m_cameraPaths.value(i).first, m_cameraPaths.value(i).second, &localFile);
-                    emit progress(i, localPath);
-                }
-            }
+        qDebug() << "Turntable position : " << QString::number(m_currentIndex);
 
-            emit done();
-        } else if(m_camera && m_camera->isConnected()) {
-            qDebug() << "capture n°" << QString::number(m_index);
-            int ret;
-            QString path = "";
-            if(!m_delayedDownload) {
-                path = m_collection->getFilePath(m_idCostume, "turntable", "jpg", m_index);
-                ret = m_camera->captureToFile(path);
-            } else {
-                qDebug() << "capture to camera";
-                QPair<QString,QString> cameraPath = m_camera->captureToCamera();
-                qDebug() << cameraPath;
-                if(cameraPath.first != "") {
-                    m_cameraPaths.insert(m_index,cameraPath);
-                    ret = GP_OK;
-                } else {
-                    ret = -1;
-                }
-            }
-            if(ret < 0) {
-                qDebug() << "Mass capture failed";
+        // The turntable is at a capture position, launch the capture if the camera
+        // is ready and this shot has not yet been done
+        if(m_readyForNextCapture && m_captureIndexes.contains(m_currentIndex)) {
+            m_readyForNextCapture = false;
+            qDebug() << "Do the capture.";
+            m_captureIndex = m_currentIndex;
+            m_lastCapturePath = m_collection->getFilePath(m_idCostume, "turntable", "jpg", m_captureIndex);
+            if(m_camera && m_camera->isConnected())
+                QtConcurrent::run(m_camera, &QPhoto::QCamera::captureToFile, m_lastCapturePath, 3);
+            else {
                 m_captureTimer->stop();
-                emit problem(CameraProblem, tr("Capture failed"));
-            } else {
-                emit progress(m_index++, path);
+                emit problem(CameraProblem, tr("Camera seems to be disconnected"));
             }
-        } else {
-            m_captureTimer->stop();
-            qDebug() << "camera problem";
-            emit problem(CameraProblem, tr("Camera seems to be disconnected"));
         }
-        qDebug() << "exiting timer timeout routine";
+        else
+            qDebug() << "Don't do the capture";
     });
+
+    // At each downloaded picture, report progress and remove captured index from the todo list
+    connect(m_camera, &QPhoto::QCamera::downloaded, [=](int status, QStringList errors) {
+        if(status == GP_OK) {
+            m_captureIndexes.remove(m_captureIndex);
+
+            emit progress(m_target - m_captureIndexes.count(), m_lastCapturePath);
+
+            if(m_captureIndexes.empty()) {
+                m_captureTimer->stop();
+                emit done();
+            }
+            else
+                m_readyForNextCapture = true;
+        }
+        else {
+            m_captureTimer->stop();
+            emit problem(CameraProblem, tr("Capture failed"));
+        }
+    });
+
     m_captureTimer->start();
 }
