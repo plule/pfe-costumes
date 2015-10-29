6a12c7e3fcf6e2d23422924b8363b4e336a5f6e8
Pierre Lulé <pierre@lule.fr>
2015-10-29 16:19:28 +0100

    Correct motors' pins numbers

--- interfaces/morphology.h
+++ interfaces/morphology.h
@@ -1,7 +1,7 @@
 /* ( pin,role,        group name,    human readable name) */
-X(   34, SIDE_MOTOR,  TOUR_POITRINE, "Tour de poitrine")
-X(   35, FRONT_MOTOR, TOUR_POITRINE, "Tour de poitrine")
-X(   36, SIDE_MOTOR,  TOUR_TAILLE,   "Tour de taille")
-X(   37, FRONT_MOTOR, TOUR_TAILLE,   "Tour de taille")
-X(   38, SIDE_MOTOR,  TOUR_HANCHE,   "Tour de hanche")
-X(   39, FRONT_MOTOR, TOUR_HANCHE,   "Tour de hanche")
+X(   7, SIDE_MOTOR,  TOUR_POITRINE, "Tour de poitrine")
+X(   6, FRONT_MOTOR, TOUR_POITRINE, "Tour de poitrine")
+X(   5, SIDE_MOTOR,  TOUR_TAILLE,   "Tour de taille")
+X(   4, FRONT_MOTOR, TOUR_TAILLE,   "Tour de taille")
+X(   3, SIDE_MOTOR,  TOUR_HANCHE,   "Tour de hanche")
+X(   2, FRONT_MOTOR, TOUR_HANCHE,   "Tour de hanche")
